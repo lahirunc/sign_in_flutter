0056515e9795984a04c58ed0e1da2a49259cf03c
Lahiru Mahagamage <lahiru.nc@gmail.com>
2021-08-25 20:26:22 +1000

    init

---
+++ lib/const.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6061FA);
+const kBackgroundColor = Color(0xFFFFFF);
+
+class Constants {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  static double? defaultSize;
+  static Orientation? orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData!.size.width;
+    screenHeight = _mediaQueryData!.size.height;
+    orientation = _mediaQueryData!.orientation;
+  }
+}
